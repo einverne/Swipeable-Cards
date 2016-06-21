c43d92211b76d7fad551116b128219e25c81da60
wujiawei <jiawei.wu@ihandysoft.com>
2016-06-21 16:06:36 +0800

    Fix click listener on Card ImageView (reverted from commit 157d93b29dc14c2e0d94b92043a85812620a1965)
    use CardContainer listener not response ImageView's click

--- AndTinder/src/main/java/com/andtinder/view/SimpleCardStackAdapter.java
+++ AndTinder/src/main/java/com/andtinder/view/SimpleCardStackAdapter.java
@@ -1,7 +1,6 @@
 package com.andtinder.view;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,40 +12,22 @@ import com.andtinder.model.CardModel;
 
 public final class SimpleCardStackAdapter extends CardStackAdapter {
 
-    CardHolder cardHolder;
+	public SimpleCardStackAdapter(Context mContext) {
+		super(mContext);
+	}
 
-    public SimpleCardStackAdapter(Context mContext) {
-        super(mContext);
-    }
+	@Override
+	public View getCardView(int position, CardModel model, View convertView, ViewGroup parent) {
+		if(convertView == null) {
+			LayoutInflater inflater = LayoutInflater.from(getContext());
+			convertView = inflater.inflate(R.layout.std_card_inner, parent, false);
+			assert convertView != null;
+		}
 
-    @Override
-    public View getCardView(int position, CardModel model, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            cardHolder = new CardHolder();
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.std_card_inner, parent, false);
+		((ImageView) convertView.findViewById(R.id.image)).setImageDrawable(model.getCardImageDrawable());
+		((TextView) convertView.findViewById(R.id.title)).setText(model.getTitle());
+		((TextView) convertView.findViewById(R.id.description)).setText(model.getDescription());
 
-            cardHolder.cardImage = (ImageView)convertView.findViewById(R.id.image);
-            convertView.setTag(cardHolder);
-            assert convertView != null;
-        } else {
-            cardHolder = (CardHolder)convertView.getTag();
-        }
-
-        ((ImageView) convertView.findViewById(R.id.image)).setImageDrawable(model.getCardImageDrawable());
-        ((TextView) convertView.findViewById(R.id.title)).setText(model.getTitle());
-        ((TextView) convertView.findViewById(R.id.description)).setText(model.getDescription());
-
-        cardHolder.cardImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("EV_TAG", "click image inner");
-            }
-        });
-        return convertView;
-    }
-
-    static class CardHolder {
-        ImageView cardImage;
-    }
+		return convertView;
+	}
 }
